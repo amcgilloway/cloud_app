095e9c9224175d65260678885762ac31f8692c38
Alastair McGilloway <alastair.mcgilloway@codeclan.com>
2022-06-23 14:04:25 +0100

    Change title

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const TitleBar = ({handleSelectChange, genres}) =>{
   return (
-    <div>
-      <h1>Hit Parade</h1>
+    <div>zw
+      <h1>Music Parade</h1>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
